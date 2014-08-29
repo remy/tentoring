4ae2ddadd106123eecd45629d8b68251893ca1e1
Fabien O'Carroll <fabien@allou.is>
2014-08-29 14:10:40 +0100

    Remove call to app.configure

--- app.js
+++ app.js
@@ -27,31 +27,29 @@ if (env === 'production') {
   app.set('url', 'http://tentoring.com');
 }
 
-app.configure(function(){
-  app.set('url', 'http://' + app.get('root'));
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.cookieParser('spa6kugo3chi4rti8wajy1no5ku'));
-  app.use(express.session({
-    cookie: { maxAge: 365 * 24 * 60 * 60 * 1000 },
-    store: new MongoStore({
-      mongoose_connection: db // jshint ignore:line
-    }),
-    secret: 'spa6kugo3chi4rti8wajy1no5ku'
-  }));
+app.set('url', 'http://' + app.get('root'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.cookieParser('spa6kugo3chi4rti8wajy1no5ku'));
+app.use(express.session({
+  cookie: { maxAge: 365 * 24 * 60 * 60 * 1000 },
+  store: new MongoStore({
+    mongoose_connection: db // jshint ignore:line
+  }),
+  secret: 'spa6kugo3chi4rti8wajy1no5ku'
+}));
 
 
-  app.use(express.static(path.join(__dirname, 'public')));
-  middleware(app);
-  app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+middleware(app);
+app.use(app.router);
 
-  app.set('title', 'Tentoring');
+app.set('title', 'Tentoring');
 
-  // app.set('tags', ['Funding', 'Legal', 'Technology', 'Design', 'Marketing', 'Product', 'Social', 'Government', 'Introductions', 'Strategy', 'Media', 'Cats']);
-});
+// app.set('tags', ['Funding', 'Legal', 'Technology', 'Design', 'Marketing', 'Product', 'Social', 'Government', 'Introductions', 'Strategy', 'Media', 'Cats']);
 
 db.on('error', console.error.bind(console, 'connection error:'));
 
