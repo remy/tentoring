3db621321107edda983d2b4be69c7d6bed68e82e
Fabien O'Carroll <fabien@allou.is>
2014-08-29 16:59:40 +0100

    Add Questions router

---
+++ routes/Questions.js
@@ -0,0 +1,75 @@
+var express = require('express');
+
+var Questions = require('../models/Questions');
+var Users = require('../models/Users');
+
+var email = require('../lib/email');
+
+var questions = express.Router();
+
+questions.param('token', function (req, res, next) {
+  Questions
+    .findOne({ token: req.params.token })
+    .populate('by')
+    .exec(function (err, question) {
+      if (err) {
+        return next(err);
+      }
+      req.question = question;
+      next();
+    });
+});
+
+questions.post('/', function (req, res, next) {
+  var now = Date.now();
+
+  var body = {
+    text: req.body.question,
+    by: req.session.user._id,
+    skill: req.body.skill
+  };
+
+  Questions
+    .create(body)
+    .exec(function (err, question) {
+
+      if (err) {
+        return next(err);
+      }
+
+      Users
+        .findOne({
+          'orgs.skills': question.skill,
+          'orgs.org': req.org._id
+        })
+        .where('orgs.asked').lt(now)
+        .ne('email', req.query.email)
+        .exec(userCallback);
+
+      function userCallback (err, user) {
+
+        if (err || !user) {
+          return next(err);
+        }
+
+        user.asked = now;
+        user.save();
+
+        email.sendQuestion({
+          user: user,
+          question: question
+        });
+
+        // TODO: This should redirect to
+        // http://tentoring.dev/questions/:questionid
+        res.render('asked');
+
+      }
+
+    });
+});
+
+questions.get('/:token', function (req, res, next) {
+});
+
+module.exports = questions;
