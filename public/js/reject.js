e049e9aec712d0fe13508bbf79d75960cfb2e6a3
Fabien O'Carroll <fabien@allou.is>
2014-09-09 19:09:01 +0100

    Add script for rejecting

---
+++ public/js/reject.js
@@ -0,0 +1,11 @@
+;(function () {
+  
+  var xhr = new XMLHttpRequest();
+  xhr.open('PUT', location.origin + '/api/questions/' + location.search.match(/id=([\d\w]+)/)[1] + '?reject=true');
+  xhr.addEventListener('loaded', function () {
+    console.log(arguments);
+    console.log('it done it');
+  });
+  xhr.send();
+
+}());
