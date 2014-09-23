6a09273952c2a54f24871447df3e4fe383ae185f
Fabien O'Carroll <fabien@allou.is>
2014-09-23 12:09:04 +0100

    Update path to cron job

--- cron/index.js
+++ cron/index.js
@@ -1,6 +1,6 @@
 var CronJob = require('cron').CronJob;
 
-var checkForUnanswered = require('../util/check-for-unanswered');
+var checkForUnanswered = require('./jobs/check-for-unanswered');
 
 exports.checkForUnanswered = new CronJob({
   cronTime: '0 0 * * * *', // Every hour on the hour, every date, every month, every day
