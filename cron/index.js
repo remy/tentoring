5f77d739a72433defc2e7b27c42daa874e52c663
Fabien O'Carroll <fabien@allou.is>
2014-09-30 14:28:28 +0100

    create cronJob for update-postponed file

--- cron/index.js
+++ cron/index.js
@@ -1,9 +1,16 @@
 var CronJob = require('cron').CronJob;
 
 var checkForUnanswered = require('./jobs/check-for-unanswered');
+var updatePostponed = require('./jobs/update-postponed.js');
 
 exports.checkForUnanswered = new CronJob({
   cronTime: '0 0 * * * *', // Every hour on the hour, every date, every month, every day
   onTick: checkForUnanswered,
+  start: false
+});
+
+exports.updatePostponed = new CronJob({
+  cronTime: '0 15 * * * *', // Every hour at quarter past
+  onTick: updatePostponed,
   start: false
 });
