61c612f6802923575fa0729e00f340d0f325293b
Fabien O'Carroll <fabien@allou.is>
2014-09-30 14:36:39 +0100

    Use exported object rather than requiring individual files

--- cron/index.js
+++ cron/index.js
@@ -1,16 +1,15 @@
 var CronJob = require('cron').CronJob;
 
-var checkForUnanswered = require('./jobs/check-for-unanswered');
-var updatePostponed = require('./jobs/update-postponed.js');
+var jobs = require('./jobs');
 
 exports.checkForUnanswered = new CronJob({
   cronTime: '0 0 * * * *', // Every hour on the hour, every date, every month, every day
-  onTick: checkForUnanswered,
+  onTick: jobs.checkForUnanswered,
   start: false
 });
 
 exports.updatePostponed = new CronJob({
   cronTime: '0 15 * * * *', // Every hour at quarter past
-  onTick: updatePostponed,
+  onTick: jobs.updatePostponed,
   start: false
 });
